3b741195ceac373961bca2ca2fb4503c44e37e94
Justin Brenner <71353662+ForgeSolutions-JBrenner@users.noreply.github.com>
2021-06-12 01:19:11 -0400

    Login and Register Updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
-import {useSelector} from 'react-redux';
+import React, { useEffect } from "react";
+import {useSelector, useDispatch} from 'react-redux';
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
-import {selectUser} from './features/userSlice.js';
+import {login, logout, selectUser} from './features/userSlice.js';
 import Login from './Login';
+import { auth } from "./firebase";
 
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userauth) => {
+      if (userauth) {
+        //user is logged in 
+        dispatch(login({
+          email: userauth.email,
+          uid: userauth.uid,
+          displayName: userauth.displayName,
+          photoURL: userauth.photoURL
+        }));
+      } else {
+        //user is logged out 
+        dispatch(logout());
+      }
+    })
+  }, []);
 
   return (
     <div className="app">
